ed39c0dc6e2acfb846c064308e0e8b6b4fe16f34
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-29 17:33:18 +0200

    Fix other typo

--- resources/views/admin/comics/create.blade.php
+++ resources/views/admin/comics/create.blade.php
@@ -10,37 +10,37 @@
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
             <input type="text" name="title" id="title" class="form-control" placeholder="Comic title here " aria-describedby="titleHelper" required>
-            <small id="titleHelper" class="text-muted">Type the name of the comic max 50 characters</small>
+            <small id="titleHelper" class="text-muted">Type the title of the comic max 100 characters</small>
         </div>
         <div class="mb-3">
-            <label for="thumb" class="form-label">thumb</label>
+            <label for="thumb" class="form-label">Thumb</label>
             <input type="text" name="thumb" id="thumb" class="form-control" placeholder="Comic thumb here " aria-describedby="thumbHelper">
-            <small id="thumbHelper" class="text-muted">Type the image of the comic max 50 characters</small>
+            <small id="thumbHelper" class="text-muted">Type the source image of the comic max 200 characters</small>
         </div>
         <div class="mb-3">
             <label for="price" class="form-label">Price</label>
             <input type="number" step="0.01" name="price" id="price" class="form-control" placeholder="Comic price here " aria-describedby="priceHelper">
-            <small id="priceHelper" class="text-muted">Type the price of the comic max 50 characters</small>
+            <small id="priceHelper" class="text-muted">Type the price of the comic max 10 characters</small>
         </div>
         <div class="mb-3">
-            <label for="series" class="form-label">series</label>
+            <label for="series" class="form-label">Series</label>
             <input type="text" name="series" id="series" class="form-control" placeholder="Comic series here " aria-describedby="seriesHelper">
-            <small id="seriesHelper" class="text-muted">Type the series of the comic max 50 characters</small>
+            <small id="seriesHelper" class="text-muted">Type the series of the comic max 100 characters</small>
         </div>
         <div class="mb-3">
-            <label for="sale_date" class="form-label">sale_date</label>
+            <label for="sale_date" class="form-label">Sale date</label>
             <input type="text" name="sale_date" id="sale_date" class="form-control" placeholder="Comic sale_date here " aria-describedby="sale_dateHelper">
-            <small id="sale_dateHelper" class="text-muted">Type the sale_date of the comic max 50 characters</small>
+            <small id="sale_dateHelper" class="text-muted">Type the sale date of the comic max 10 characters</small>
         </div>
         <div class="mb-3">
-            <label for="type" class="form-label">type</label>
+            <label for="type" class="form-label">Type</label>
             <input type="text" name="type" id="type" class="form-control" placeholder="Comic type here " aria-describedby="typeHelper">
-            <small id="typeHelper" class="text-muted">Type the type of the comic max 50 characters</small>
+            <small id="typeHelper" class="text-muted">Type the type of the comic max 100 characters</small>
         </div>
         <div class="mb-3">
             <label for="description" class="form-label">Description</label>
             <input type="text" name="description" id="description" class="form-control" placeholder="Comic description here " aria-describedby="descriptionHelper">
-            <small id="descriptionHelper" class="text-muted">Type the description of the comic max 50 characters</small>
+            <small id="descriptionHelper" class="text-muted">Type the description of the comic max 1000 characters</small>
         </div>
         <button type="submit" class="btn btn-dark w-100 my-4">Save</button>
     </form>
