a6402a8d3d7e9a632e1025db4850eaf39161b1df
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-29 17:20:10 +0200

    Import ComicController

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Guest\PageController;
+use App\Http\Controllers\Admin\ComicController;
+
+/*
 
 /*
 |--------------------------------------------------------------------------
