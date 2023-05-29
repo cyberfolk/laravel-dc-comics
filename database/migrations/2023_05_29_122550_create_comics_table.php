5c4b88608b3c1f3671772f2b091a457dc5bc4a46
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-29 15:15:34 +0200

    Popolate db

--- database/migrations/2023_05_29_122550_create_comics_table.php
+++ database/migrations/2023_05_29_122550_create_comics_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->string('description', 1000);
+            $table->string('thumb', 200);
+            $table->string('price', 10);
+            $table->string('series', 100);
+            $table->string('sale_date', 100);
+            $table->string('type', 100);
+            $table->string('artists', 100)->nullable();
+            $table->string('writers', 100)->nullable();
             $table->timestamps();
         });
     }
