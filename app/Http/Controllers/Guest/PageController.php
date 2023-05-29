cd043656b878616de756fb8b21e32a525e36bc65
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-29 15:55:56 +0200

    Set Guest/PageController and web

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,9 +3,67 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function tv()
+    {
+        $pages = config('pages');
+        return view('tv', compact('pages'));
+    }
+    public function shop()
+    {
+        $pages = config('pages');
+        return view('shop', compact('pages'));
+    }
+    public function news()
+    {
+        $pages = config('pages');
+        return view('news', compact('pages'));
+    }
+    public function fans()
+    {
+        $pages = config('pages');
+        return view('fans', compact('pages'));
+    }
+    public function games()
+    {
+        $pages = config('pages');
+        return view('games', compact('pages'));
+    }
+    public function videos()
+    {
+        $pages = config('pages');
+        return view('videos', compact('pages'));
+    }
+    public function movies()
+    {
+        $pages = config('pages');
+        return view('movies', compact('pages'));
+    }
+    public function characters()
+    {
+        $pages = config('pages');
+        return view('characters', compact('pages'));
+    }
+    public function collectibles()
+    {
+        $pages = config('pages');
+        return view('collectibles', compact('pages'));
+    }
+    public function home()
+    {
+        $pages = config('pages');
+        return view('home', compact('pages'));
+    }
+    public function comics()
+    {
+        $comics = Comic::all();
+        $pages = config('pages');
+        $banner = config('banner');
+        $pages = config('pages');
+        return view('comics', compact('comics', 'pages', 'banner'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,60 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::get('/', function () {
-    $pages = config('pages');
-    return view('characters', compact('pages'));
-})->name('home');
-
-Route::get('/comics', function () {
-    $comics = config('comics');
-    $banner = config('banner');
-    $pages = config('pages');
-    return view('comics', compact('comics', 'pages', 'banner'));
-})->name('comics');
-
-Route::get('/characters', function () {
-    $pages = config('pages');
-    return view('characters', compact('pages'));
-})->name('characters');
-
-Route::get('/movies', function () {
-    $pages = config('pages');
-    return view('movies', compact('pages'));
-})->name('movies');
-
-Route::get('/tv', function () {
-    $pages = config('pages');
-    return view('tv', compact('pages'));
-})->name('tv');
-
-Route::get('/games', function () {
-    $pages = config('pages');
-    return view('games', compact('pages'));
-})->name('games');
-
-Route::get('/collectibles', function () {
-    $pages = config('pages');
-    return view('collectibles', compact('pages'));
-})->name('collectibles');
-
-Route::get('/videos', function () {
-    $pages = config('pages');
-    return view('videos', compact('pages'));
-})->name('videos');
-
-Route::get('/fans', function () {
-    $pages = config('pages');
-    return view('fans', compact('pages'));
-})->name('fans');
-
-Route::get('/news', function () {
-    $pages = config('pages');
-    return view('news', compact('pages'));
-})->name('news');
-
-Route::get('/shop', function () {
-    $pages = config('pages');
-    return view('shop', compact('pages'));
-})->name('shop');
+Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('/collectibles', [PageController::class, 'collectibles'])->name('collectibles');
+Route::get('/characters', [PageController::class, 'characters'])->name('characters');
+Route::get('/comics', [PageController::class, 'comics'])->name('comics');
+Route::get('/movies', [PageController::class, 'movies'])->name('movies');
+Route::get('/videos', [PageController::class, 'videos'])->name('videos');
+Route::get('/games', [PageController::class, 'games'])->name('games');
+Route::get('/fans', [PageController::class, 'fans'])->name('fans');
+Route::get('/news', [PageController::class, 'news'])->name('news');
+Route::get('/shop', [PageController::class, 'shop'])->name('shop');
+Route::get('/tv', [PageController::class, 'tv'])->name('tv');
