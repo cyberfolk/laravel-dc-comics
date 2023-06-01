65b8c5d815e3e75d392c4265ab0f68f88b18a674
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-01 16:35:37 +0200

    Add custom message error and add chek to date

--- app/Http/Requests/StoreComicRequest.php
+++ app/Http/Requests/StoreComicRequest.php
@@ -29,10 +29,33 @@ class StoreComicRequest extends FormRequest
             'thumb' =>  'nullable|max:200', //image?
             'price' =>  'required|decimal:0,2',
             'series' =>  'required|max:100',
-            'sale_date' =>  'nullable|date',
+            'sale_date' =>  'nullable|date|before_or_equal:today|after:1900-01-01",',
             'type' =>  'required|max:100',
             'artists' => 'nullable',
             'writers' => 'nullable',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'title.required' => 'The title is required',
+            'title.max' => 'The title must contain no more than 100 characters',
+            'thumb.max' => 'The thumb must contain no more than 200 characters',
+            'price.required' => 'The price is required',
+            'price.decimal' => 'The price must contain no more than 2 decimal digits',
+            'series.max' => 'The series is required',
+            'series.max' => 'The series must contain no more than 100 characters',
+            /*             'sale_date.date' => 'The sale date must be data type',
+            'sale_date.before_or_equal' => 'The sale date must be no more than today',
+            'sale_date.after' => 'The sale date must be after 1900-01-01',
+ */            'type' => 'The type is required',
+            'type.max' => 'The type must contain no more than 100 characters',
         ];
     }
 }
--- app/Http/Requests/UpdateComicRequest.php
+++ app/Http/Requests/UpdateComicRequest.php
@@ -35,4 +35,27 @@ class UpdateComicRequest extends FormRequest
             'writers' => 'nullable',
         ];
     }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'title.required' => 'The title is required',
+            'title.max' => 'The title must contain no more than 100 characters',
+            'thumb.max' => 'The thumb must contain no more than 200 characters',
+            'price.required' => 'The price is required',
+            'price.decimal' => 'The price must contain no more than 2 decimal digits',
+            'series.max' => 'The series is required',
+            'series.max' => 'The series must contain no more than 100 characters',
+            'sale_date.date' => 'The sale date is not a valid date',
+            'sale_date.before_or_equal' => 'The sale date must be no more than today',
+            'sale_date.after' => 'The sale date must be after 1900-01-01',
+            'type' => 'The type is required',
+            'type.max' => 'The type must contain no more than 100 characters',
+        ];
+    }
 }
